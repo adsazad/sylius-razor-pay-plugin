8d68ff56305f99ca606f9a7ea595a047f81b56bc
Kannan Seeni <kannanseeni@gmail.com>
2020-10-02 21:16:16 +0530

    first update

---
+++ src/SrkitsSyliusRazorPayPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Srkits\SyliusRazorPayPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SrkitsSyliusRazorPayPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}

d120383c52a0ad51e39d7e0c298a768f31bf5b45
srkits <md@srkitsolutions.com>
2020-10-05 13:18:44 +0530

    added capture action

--- src/Payum/SyliusPaymentGatewayFactory.php
+++ src/Payum/SyliusPaymentGatewayFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Srkits\SyliusRazorPayPlugin\Payum;
 
+use Srkits\SyliusRazorPayPlugin\Payum\Action\StatusAction;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayFactory;
 
@@ -14,6 +15,7 @@ final class SyliusPaymentGatewayFactory extends GatewayFactory
         $config->defaults([
             'payum.factory_name' => 'sylius_payment',
             'payum.factory_title' => 'Sylius Payment',
+            'payum.action.status' => new StatusAction(),
         ]);
 
         $config['payum.api'] = function (ArrayObject $config) {
